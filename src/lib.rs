f82fec212efca7c464bf3ea9f7bc98c3ccc5c7c2
Onur Yildiz <oya4@sfu.ca>
2026-02-27 00:58:25 -0800

    Added support for jal, jalr, and lui

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,8 @@ impl RiscvCpu {
             0x03 => self.handle_load(instruction),
             0x23 => self.handle_store(instruction),
             0x63 => self.handle_btype(instruction, &mut next_pc),
+            0x6F => self.handle_jal(instruction, &mut next_pc),
+            0x67 => self.handle_jalr(instruction, &mut next_pc),
             _ => println!("don't have this yet"),
         }
 
@@ -129,11 +131,6 @@ impl RiscvCpu {
         let imm = (instruction as i32) >> 20;
         let rs_value = self.regs[rs as usize];
 
-        println!("rd: {:#07x}", rd);
-        println!("Fuct3: {:#03x}", funct3);
-        println!("rs: {:#07x}", rs);
-        println!("Sign Extended Imm: {:#034x}", imm);
-
         let rd_value = match funct3 {
             0x0 => (rs_value as i32 + imm) as u32,
             0x4 => rs_value ^ (imm as u32),
@@ -217,10 +214,10 @@ impl RiscvCpu {
         let rs1 = (instruction >> 15) & 0x1F;
         let rs2 = (instruction >> 20) & 0x1F;
 
-        let b4_0 = (instruction >> 7) & 0x1F;
-        let b11_5 = (instruction >> 25) & 0x7F;
+        let i4_0 = (instruction >> 7) & 0x1F;
+        let i11_5 = (instruction >> 25) & 0x7F;
 
-        let imm_u: u32 = (b11_5 << 5) | b4_0;
+        let imm_u: u32 = (i11_5 << 5) | i4_0;
         let imm = ((imm_u << 20) as i32) >> 20;
 
         let rs1_value = self.regs[rs1 as usize];
@@ -234,7 +231,6 @@ impl RiscvCpu {
             0x2 => self.store(addr, MemSize::Word, rs2_value),
             _ => panic!("Unknown S-type funct3: {:#x}", funct3),
         }
-
     }
 
     pub fn handle_btype(&mut self, instruction: u32, next_pc: &mut u32) {
@@ -242,12 +238,12 @@ impl RiscvCpu {
         let rs1 = (instruction >> 15) & 0x1F;
         let rs2 = (instruction >> 20) & 0x1F;
 
-        let b12 = (instruction >> 31) & 0x1;
-        let b11 = (instruction >> 7) & 0x1;
-        let b10_5 = (instruction >> 25) & 0x3F;
-        let b4_1 = (instruction >> 8) & 0xF;
+        let i12 = (instruction >> 31) & 0x1;
+        let i11 = (instruction >> 7) & 0x1;
+        let i10_5 = (instruction >> 25) & 0x3F;
+        let i4_1 = (instruction >> 8) & 0xF;
 
-        let imm_u32 = (b12 << 12) | (b11 << 11) | (b10_5 << 5) | (b4_1 << 1);
+        let imm_u32 = (i12 << 12) | (i11 << 11) | (i10_5 << 5) | (i4_1 << 1);
         let imm = ((imm_u32 << 19) as i32) >> 19;
 
         let rs1_value = self.regs[rs1 as usize];
@@ -266,6 +262,47 @@ impl RiscvCpu {
         if should_branch {
             *next_pc = (self.pc as i32).wrapping_add(imm) as u32;
         }
+    }
+
+    pub fn handle_jal(&mut self, instruction: u32, next_pc: &mut u32) {
+        let rd = (instruction >> 7) & 0x1F;
+
+        let i19_12 = (instruction >> 12) & 0xFF;
+        let i11 = (instruction >> 20) & 0x1;
+        let i10_1 = (instruction >> 21) & 0x3FF;
+        let i20 = (instruction >> 31) & 0x1;
+
+        let imm_u32 = (i20 << 20) | (i19_12 << 12) | (i11 << 11) | (i10_1 << 1);
+        let imm = ((imm_u32 << 11) as i32) >> 11;
+
+        let rd_value = self.pc + 4;
+        self.write_reg(rd, rd_value);
+        *next_pc = (self.pc as i32).wrapping_add(imm) as u32;
+    }
+
+    pub fn handle_jalr(&mut self, instruction: u32, next_pc: &mut u32) {
+        let rd = (instruction >> 7) & 0x1F;
+        let funct3 = (instruction >> 12) & 0x7;
+        let rs = (instruction >> 15) & 0x1F;
+        let imm = (instruction as i32) >> 20;
+        let rs_value = self.regs[rs as usize];
+
+        let rd_value = self.pc + 4;
+
+        match funct3 {
+            0x0 => *next_pc = (rs_value as i32).wrapping_add(imm) as u32,
+            _ => panic!("Unknown JALR funct3 {:#x}", funct3),
+        }
+
+        self.write_reg(rd, rd_value);
+    }
+
+    pub fn handle_lui(&mut self, instruction: u32) {
+        let rd = (instruction >> 7) & 0x1F;
+        let imm = (instruction >> 12) & 0xFFFFF;
+
+        let rd_value = imm << 12;
+        self.write_reg(rd, rd_value);
     }
 
     fn fetch(&self) -> u32 {
